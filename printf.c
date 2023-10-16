af8c5a58186d7476fe4228e422436bf44ba7f6de
daverobeny <nyamwangedavid2019@gmail.com>
2023-10-17 00:21:10 +0300

    commit

--- printf.c
+++ printf.c
@@ -1,71 +1,65 @@
 #include "main.h"
 
-
 int _printf(const char *format, ...)
 {
-    int num_char = 0;
+	int num_char = 0;
 
-    va_list arg_list;
+	va_list arg_list;
 
-    if(!format)
-    {
-        return (-1);
-    }
+	if (!format)
+	{
+		return (-1);
+	}
 
-    var_start(arg_list, format);
+	var_start(arg_list, format);
 
-    {
-        while(*format != '\0')
-        {
-            if (*format != '%')
-            {
-                write(1, &format, 1);
-                num_char++;
-                format++;
-        //check for %(it is not there)
-        // writes output to the stdio
-        //return number of characters printed
-            }
-            else
-            {
-                format++;
-                if (format = 'c')
-                {
-                    char c = va_arg(arg_list, char);
-                    write(1, &c, 1);
-                    num_char++;
-                }
-                else if (format = '%')
-                {
-                    char c = va_arg(arg_list, char);
-                    write(1, &c, 1);
-                    num_char++;
-                }
-                else
-                {
-                    char *str = va_arg(arg_list, char*);
-                    int len;
-		    for (len = 0; str[len] != '\0'; len++)
-		    {
-			    _putchar(str);
-			    num_char++;
+	while (*format != '\0')
+	{
+		if (*format != '%')
+		{
+			write(1, &format, 1);
+			num_char++;
+			format++;
+			// check for %(it is not there)
+			//  writes output to the stdio
+			// return number of characters printed
+		}
+		else
+		{
+			format++;
+			if (format = 'c')
+			{
+				char c = va_arg(arg_list, char);
+				write(1, &c, 1);
+				num_char++;
+			}
+			else if (format = '%')
+			{
+				char c = va_arg(arg_list, char);
+				write(1, &c, 1);
+				num_char++;
+			}
+			else
+			{
+				char *str = va_arg(arg_list, char *);
+				int len;
+				for (len = 0; str[len] != '\0'; len++)
+				{
+					_putchar(str);
+					num_char++;
+				}
+			}
+			// check for %(exists)
+			// check if the next character is c, s, or %
+			// if c va_arg
+			//  if null
+			//  if string. count characters and also print
+			//  writes output to the stdio
+			// return number of characters printed
+		}
+		format++;
+	}
 
-		    }
-                }
-        //check for %(exists)
-        //check if the next character is c, s, or %
-        //if c va_arg
-        // if null
-        // if string. count characters and also print
-        // writes output to the stdio
-        //return number of characters printed
-            }
-            formart++;
-        }
-
-    }
-
-
-    va_end(arg_list);
-    return (num_char);
+	va_end(arg_list);
+	return (num_char);
 }
