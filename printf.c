36232eaa7eb343ebd4567f747e774a8d52005228
daverobeny <nyamwangedavid2019@gmail.com>
2023-10-17 20:55:09 +0300

    printf

--- printf.c
+++ printf.c
@@ -18,7 +18,10 @@ int _printf(const char *format, ...)
 	{
 		return (-1);
 	}
-
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+	{
+		return (-1);
+	}
 	va_start(args, format);
 
 	while (*format)
