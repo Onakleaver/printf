452af19fe91cd4e76fdfd51f3444f11208649d38
daverobeny <nyamwangedavid2019@gmail.com>
2023-10-20 02:57:50 +0300

    printf

--- printf.c
+++ printf.c
@@ -11,95 +11,84 @@
 
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int printed_chars = 0;
+    va_list args;
+    int printed_chars = 0;
 
-	if (!format || (format[0] == '%' && !format[1]))
-	{
-		return (-1);
-	}
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-	{
-		return (-1);
-	}
-	va_start(args, format);
+    if (!format || (format[0] == '%' && !format[1]))
+    {
+        return (-1);
+    }
+    if (format[0] == '%' && format[1] == ' ' && !format[2])
+    {
+        return (-1);
+    }
+    va_start(args, format);
 
-	while (*format)
-	{
-		if (*format != '%')
-		{
-			write(1, format, 1);
-			printed_chars++;
-		}
-		else
-		{
-			format++;
-			switch (*format)
-			{
-			case 'c':
-			{
-				char c = va_arg(args, int);
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            write(1, format, 1);
+            printed_chars++;
+        }
+        else
+        {
+            format++;
+            char flags = 0;
+            while (*format == '+' || *format == '-' || *format == ' ')
+            {
+                flags |= (*format == '+') ? 1 : 0;
+                flags |= (*format == '-') ? 2 : 0;
+                flags |= (*format == ' ') ? 4 : 0;
+                format++;
+            }
 
-				write(1, &c, 1);
-				printed_chars++;
-				break;
-			}
-			case 's':
-			{
-				char *str = va_arg(args, char *);
+            switch (*format)
+            {
+            case 'd':
+            case 'i':
+            {
+                int num = va_arg(args, int);
+                char buffer[20];
+                int len = snprintf(buffer, sizeof(buffer), "%+d", num); // Handle '+' flag
+                if (flags & 2)
+                {
+                    // Handle '-' flag
+                    write(1, buffer, len);
+                    printed_chars += len;
+                    int padding = 0;
+                    while (padding++ < 10 - len)
+                    {
+                        write(1, " ", 1);
+                        printed_chars++;
+                    }
+                }
+                else if (flags & 4)
+                {
+                    // Handle ' ' flag
+                    write(1, " ", 1);
+                    printed_chars++;
+                    int padding = 0;
+                    while (padding++ < 10 - len)
+                    {
+                        write(1, " ", 1);
+                        printed_chars++;
+                    }
+                }
+                else
+                {
+                    write(1, buffer, len);
+                    printed_chars += len;
+                }
+                break;
+            }
+            default:
+                break;
+            }
+        }
+        format++;
+    }
 
-				while (*str != '\0')
-				{
-					write(1, str, 1);
-					str++;
-					printed_chars++;
-				}
-				break;
-			}
-			case 'd': 
-			case 'i':
-			{
-				int num = va_arg(args, int);
-				char buffer[20];
-				int len = snprintf(buffer, sizeof(buffer), "%d", num);
-
-				write(1, buffer, len);
-				printed_chars += len;
-				break;
-			}
-			case 'u':
-			{
-				unsigned int num = va_arg(args, unsigned int);
-				char buffer[20];
-				int len = snprintf(buffer, sizeof(buffer), "%u", num);
-
-				write(1, buffer, len);
-				printed_chars += len;
-				break;
-			}
-			case 'p':
-			{
-				void *ptr = va_arg(args, void *);
-				char buffer[20];
-				int len = snprintf(buffer, sizeof(buffer), "%p", ptr);
-
-				write(1, buffer, len);
-				printed_chars += len;
-				break;
-			}
-			case '%':
-			{
-				write(1, "%", 1);
-				printed_chars++;
-				break;
-			}
-			default:
-				break;
-			}
-		}
-		format++;
-	}
-
-	va_end(args);
-	return (printed_chars);
+    va_end(args);
+    return (printed_chars);
 }
