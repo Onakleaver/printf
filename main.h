0d9e423e3b78348fe792a7cf15e340054d54c90e
daverobeny <nyamwangedavid2019@gmail.com>
2023-10-20 02:42:46 +0300

    printf

--- main.h
+++ main.h
@@ -7,5 +7,10 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int handle_specifier(char specifier, va_list args);
+int _strlen(const char *str);
+int _itoa(int num, char *buffer);
+int _utoa(unsigned int num, char *buffer);
+int _ptoa(void *ptr, char *buffer);
 
 #endif
